1adcbf42d155dbb51320515224ab9169696cd78d
J. Caleb Wherry <caleb@calebwherry.com>
2015-02-11 12:27:56 -0500

    First commit.

---
+++ prime-factors/FileIOLib.h
@@ -0,0 +1,43 @@
+///////////////////////////////////////
+///
+///	\file		FileIOLib.h
+///	\author		J. Caleb Wherry
+///	\date		2/11/2015
+///
+///////////////////////////////////////
+
+//
+// Local includes:
+//
+// N/A
+
+
+//
+// Compiler includes:
+//
+#include <iostream>
+
+
+//
+// Namespaces:
+//
+using namespace std;
+
+
+//
+// Main library namespace:
+//
+namespace file_io
+{
+
+	/// RAII File class
+	class FileIO
+	{
+	public:
+
+
+	private:
+
+	};
+
+} // namespace file_io
---
+++ prime-factors/prime-factors.cpp
@@ -0,0 +1,37 @@
+///////////////////////////////////////
+///
+///	\file		prime-factors.cpp
+///	\author		J. Caleb Wherry
+///	\date		2/11/2015
+///
+///////////////////////////////////////
+
+
+//
+// Local includes:
+//
+#include "FileIOLib.h"
+
+
+//
+// Compiler includes:
+//
+#include <iostream>
+
+
+//
+// Namespaces:
+//
+using namespace std;
+namespace fio = file_io;
+
+
+//
+// Main:
+//
+int main()
+{
+	cout << "Hello, world!" << endl;
+
+	return 0;
+}
